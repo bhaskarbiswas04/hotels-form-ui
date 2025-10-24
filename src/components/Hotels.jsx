674cf959ac266264d2f984d1803c5b5fcbba0ab7
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2025-10-24 10:13:52 +0530

    Updated Hotels.jsx component

--- src/components/Hotels.jsx
+++ src/components/Hotels.jsx
@@ -1,11 +1,33 @@
 //
+import { useState } from "react";
 import useFetch from "../useFetch"
 
 const Hotels = ()=>{
-
+   const [successMessage, setSuccessMessage] = useState("");
    const { data, loading, error } = useFetch(
      "https://hotels-backend-rosy.vercel.app/hotels"
    );
+
+   const handleDelete = async (hotelId)=>{
+    try {
+      const response = await fetch(
+        `https://hotels-backend-rosy.vercel.app/hotels/${hotelId}`,
+        { method: "DELETE" }
+      );
+
+      if(!response.ok) {
+        throw "Failed to delete Hotel."
+      }
+
+      const data = await response.json();
+      if(data) {
+        setSuccessMessage("Hotel data deleted successfully.")
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log("Failed to delete Hotel data from database.")
+    }
+   }
 
    if (loading) return <p>Loading...</p>;
    if (error) return <p>Error fetching hotels</p>;
@@ -15,7 +37,10 @@ const Hotels = ()=>{
        <h2>List of all Hotels</h2>
        <ul>
          {data?.map((hotel) => (
-           <li key={hotel._id}>{hotel.name}</li>
+           <li key={hotel._id}>
+             {hotel.name}{" "}
+             <button onClick={() => handleDelete(hotel._id)}>Delete</button>
+           </li>
          ))}
        </ul>
      </div>
